1e6350e5d5f795ec74f5c9ca1bff6903bb4d9ff2
hnguyen446 <hienngominh.nguyen2@dxc.com>
2024-03-12 20:05:13 +0700

    update

--- src/main/java/com/example/backend/helpers/HttpServices.java
+++ src/main/java/com/example/backend/helpers/HttpServices.java
@@ -16,30 +16,39 @@ import java.util.List;
 
 public class HttpServices {
 
-    static String link = "https://www.nettruyenbing.com/";
+    static String link = "https://www.nettruyenee.com/";
 
     static HttpClient client = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(10))
             .build();
 
+    private static String urlWeb(String url) {
+//        if (url.contains("?page=1")) {
+//            url = url.replace("?page=1", "");
+//        }
+//        return "https://webcache.googleusercontent.com/search?q=cache:" + url;
+        return url;
+    }
+
     public static String getResponseString(String url, String referer) throws IOException, InterruptedException {
-        System.out.println(url);
         HttpResponse response = null;
-        URI uri = URI.create(url);
+        System.out.println(urlWeb(url));
+        URI uri = URI.create(urlWeb(url));
         HttpRequest request = HttpRequest.newBuilder()
                 .header("Referer", referer == null ? link : referer)
-//                .header("User-Agent","Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Mobile Safari/537.36")
+                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 //                .header("cookie","_ga=GA1.2.1060682002.1662527420; _gid=GA1.2.1724377404.1664337415; popup_window=1; _gat_gtag_UA_57670566_6=1")
                 .uri(uri).build();
         response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.headers().toString());
         return response.body().toString();
     }
 
     public static byte[] getResponseByte(String url) throws IOException, InterruptedException {
         HttpResponse response = null;
-        URI uri = URI.create(url);
+        URI uri = URI.create(urlWeb(url));
         HttpRequest request = HttpRequest.newBuilder()
                 .header("Referer", link)
-//                .header("User-Agent","Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Mobile Safari/537.36")
+                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 //                .header("cookie","_ga=GA1.2.1060682002.1662527420; _gid=GA1.2.1724377404.1664337415; popup_window=1; _gat_gtag_UA_57670566_6=1")
                 .uri(uri).build();
         response = client.send(request, HttpResponse.BodyHandlers.ofByteArray());
@@ -49,10 +58,10 @@ public class HttpServices {
     public static List<String> getListSrcImg(String url) throws IOException, InterruptedException {
         List<String> result = new ArrayList<>();
         HttpResponse response = null;
-        URI uri = URI.create(url);
+        URI uri = URI.create(urlWeb(url));
         HttpRequest request = HttpRequest.newBuilder()
                 .header("Referer", link)
-//                .header("User-Agent","Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Mobile Safari/537.36")
+                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 //                .header("cookie","_ga=GA1.2.1060682002.1662527420; _gid=GA1.2.1724377404.1664337415; popup_window=1; _gat_gtag_UA_57670566_6=1")
                 .uri(uri).build();
         response = client.send(request, HttpResponse.BodyHandlers.ofString());
